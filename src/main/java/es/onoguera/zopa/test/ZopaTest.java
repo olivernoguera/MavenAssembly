090ec4e651e71437596c878f4a29eea296400ed5
Oliver Noguera <onoguera@oneboxtm.com>
2018-02-10 15:31:38 +0100

    INitial commit

---
+++ src/main/java/es/onoguera/zopa/test/ZopaTest.java
@@ -0,0 +1,12 @@
+package es.onoguera.zopa.test;
+
+import static java.lang.System.exit;
+
+public class ZopaTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        exit(0);
+    }
+}
